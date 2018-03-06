d755a6b8824a649ecd57bccdd4d90be66624fa2e
Rima Jean <rima.jean@gmail.com>
2018-03-06 18:08:25 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,7 +6,12 @@ def initialize(word)
 end
 
 def match(word_array)
-
+  word_array.select do |string|
+    string.split(",")
+    char_array = string.split('')
+    split_words = word.split('')
+    char_array.sort == split_words.sort
+  end
 end
 
 end
